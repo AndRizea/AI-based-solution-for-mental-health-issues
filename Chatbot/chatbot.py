4875d0f8e48fde4550ba6a9f96a06616c16cdfe3
Andreea Rizea <andreearizea18@yahoo.com>
2022-03-15 20:27:05 +0200

    New commit

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -5,24 +5,15 @@ from nltk.stem.lancaster import LancasterStemmer
 
 
 # Textual sentiment analysis
-anger_training_set = []
-fear_training_set = []
-sadness_training_set = []
-happiness_training_set = []
-
-anger_test_set = []
-fear_test_set = []
-sadness_test_set = []
-happiness_test_set = []
-
 stemmer = LancasterStemmer()
 
 def load_training_data(sentiment):
-    data = open("../Data/" + sentiment + "_training_set.txt", encoding="utf8")
+    data = open("Data/" + sentiment + "_training_set.txt", encoding="utf8")
+    threshold = 0
     if sentiment == "anger":
         threshold = 0.5
     elif sentiment == "fear":
-        threshold = 0.5
+        threshold = 0.6
     elif sentiment == "sadness":
         threshold = 0.5
     elif sentiment == "happiness":
@@ -31,13 +22,13 @@ def load_training_data(sentiment):
     return data, threshold
 
 def load_test_data(sentiment):
-    data = open("../Data/" + sentiment + "_training_set.txt", encoding="utf8")
+    data = open("Data/" + sentiment + "_training_set.txt", encoding="utf8")
     return data
 
 def clean_data(training_data, threshold):
     training_set = []
     for line in training_data:
-        line = line.strip().lower #strip() - remove spaces at the beginning and at the end of the string
+        line = line.strip().lower() #strip() - remove spaces at the beginning and at the end of the string
         if line.split()[-1] == "none": #last word in the line
             line = " ".join(filter(lambda x: x[0] != '@', line.split()))
             punctuation = line.maketrans("", "", '.*%$^0123456789#!][\?&/)/(+-<>')
@@ -83,7 +74,34 @@ def bag_of_words(data):
     return training_set, all_words
 
 
+load_anger_training_data, threshold = load_training_data("anger")
+print("***************************Loading data for anger**************************")
+anger_training_set = clean_data(load_anger_training_data, threshold)
+print(len(anger_training_set))
 
+load_fear_training_data, threshold = load_training_data("fear")
+print("***************************Loading data for fear**************************")
+fear_training_set = clean_data(load_fear_training_data, threshold)
+
+load_sadness_training_data, threshold = load_training_data("sadness")
+print("***************************Loading data for sadness**************************")
+sadness_training_set = clean_data(load_sadness_training_data, threshold)
+
+load_joy_training_data, threshold = load_training_data("joy")
+print("***************************Loading data for joy**************************")
+joy_training_set = clean_data(load_joy_training_data, threshold)
+
+load_anger_test_data = load_test_data("anger")
+anger_test_data = clean_data(load_anger_test_data, threshold)
+
+load_fear_test_data = load_test_data("fear")
+fear_test_data = clean_data(load_fear_test_data, threshold)
+
+load_sadness_test_data = load_test_data("sadness")
+sadness_test_data = clean_data(load_sadness_test_data, threshold)
+
+load_joy_test_data = load_test_data("joy")
+joy_test_data = clean_data(load_joy_test_data, threshold)
 
 
 
